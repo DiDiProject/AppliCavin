36a36797d6e5746e5e28615bf7a4b99cf17e1af3
CamillePar <camille.parillaud@gmail.com>
2015-11-05 14:56:33 +0100

    Test commit cam

--- Exemples/app/src/main/java/com/example/didi/exemples/MainActivity.java
+++ Exemples/app/src/main/java/com/example/didi/exemples/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends Activity {
         menuPrincipal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Le contenu du menu principal va s'afficher !", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Retestons Le contenu du menu principal va s'afficher !", Toast.LENGTH_SHORT).show();
                 Intent n = new Intent(MainActivity.this, AffichageMenuPrincipal.class);
                 startActivity(n);
             }
